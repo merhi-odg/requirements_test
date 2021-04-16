7e345b14a7ff108ab0f6c3877ff0c861e52bc60e
merhi-odg <51339977+merhi-odg@users.noreply.github.com>
2021-04-16 15:46:47 -0500

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,11 @@ import aif360
 
 # modelop.init
 def begin():
+    
+    global version
+    version = aif360.__version__
+    
+    print("version: ", version, flush=True)
     pass
 
 # modelop.score
