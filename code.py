b9ac408ad63b96520bad2660daefd94d68273321
merhi-odg <51339977+merhi-odg@users.noreply.github.com>
2021-04-20 09:14:37 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,17 +1,17 @@
-import aif360
-import statsmodels
+# import aif360
+# import statsmodels
 
 # modelop.init
 def begin():
     
-    global aif360_version
-    global statsmodels_version
+    # global aif360_version
+    # global statsmodels_version
 
-    aif360_version = aif360.__version__
-    statsmodels_version = statsmodels.__version__
+    # aif360_version = aif360.__version__
+    # statsmodels_version = statsmodels.__version__
     
-    print("aif360_version: ", vaif360ersion, flush=True)
-    print("statsmodels_version: ", statsmodels_version, flush=True)
+    # print("aif360_version: ", vaif360ersion, flush=True)
+    # print("statsmodels_version: ", statsmodels_version, flush=True)
 
     pass
 
